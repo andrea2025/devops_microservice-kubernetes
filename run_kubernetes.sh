2b9659145d5f0491696f67777f6d964609eac5d1
Ubuntu <ubuntu@ip-172-31-10-118.ec2.internal>
2023-02-25 16:39:13 +0000

    microservice-kubernetes

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="andrea45/udademoproject1"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run udademoproject1 --image=$dockerpath --port=80 --labels app=udademoproject
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward udademoproject1 8000:80
